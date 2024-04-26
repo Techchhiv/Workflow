731cd5ac32390dcce093bb0185669df2978f0b46
Techchhiv <techchivlim5@gmail.com>
2024-04-26 15:25:48 +0700

    init

---
+++ src/main/java/i4/gicb/App.java
@@ -0,0 +1,13 @@
+package i4.gicb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
